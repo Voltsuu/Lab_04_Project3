75b85e14c37789cad0d495ba487bfa5ed667b80d
Voltsuuu <npoudel2007@gmail.com>
2023-09-19 21:38:43 -0400

    Lab_04_Project3

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int springMaintenance;
+        int summerMaintenance;
+        int fallMaintenance;
+        int winterMaintenance;
+
+        System.out.println("What is the cost of your spring maintenance?");
+        springMaintenance = scan.nextInt();
+        System.out.println("What is the cost of your summer maintenance?");
+        summerMaintenance = scan.nextInt();
+        System.out.println("What is the cost of your fall maintenance?");
+        fallMaintenance = scan.nextInt();
+        System.out.println("What is the cost of your winter maintenance?");
+        winterMaintenance = scan.nextInt();
+
+        int total = springMaintenance + summerMaintenance + fallMaintenance + winterMaintenance;
+
+        System.out.println("Your yearly maintenance cost is $ " + total);
+    }
+}
